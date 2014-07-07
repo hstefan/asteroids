85fbba9b96b016846ce9e99d749dcacb348be33c
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-07-06 23:05:02 -0300

    Importing ship.sh script.

---
+++ ship.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+TITLE="Asteroids"
+zip -9 -r $TITLE.love *.lua data/ hump/
+mv ${TITLE}.love redist/$TITLE
+cd redist/$TITLE
+cat love.exe ${TITLE}.love > ${TITLE}.exe
+cd ..
+zip -r ${TITLE}.zip ${TITLE} -x $TITLE/love.exe
+cp ${TITLE}.zip ~/Copy/Share/.
+
