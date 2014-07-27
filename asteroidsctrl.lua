9c8cdcdbe2b0e38c0d5d762670f6e90a2bc2ddfc
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-07-27 05:43:11 -0300

    Initial version of asteroid spawning.

---
+++ asteroidsctrl.lua
@@ -0,0 +1,55 @@
+Class = require "hump.class"
+Vector = require "hump.vector"
+Shapes = require "shapes"
+
+local function randomAsteroid()
+	local angle = 2 * math.pi * math.random()
+	local asteroid = {}
+	asteroid.vel = math.random(100, 180) * Vector(math.cos(angle), math.sin(angle))
+	asteroid.r = math.random(20, 80)
+	local w, h = love.window.getDimensions()
+
+	if angle < math.pi/2 then
+		asteroid.pos = Vector(-asteroid.r, -asteroid.r)
+	elseif angle < math.pi then
+		asteroid.pos = Vector(w + asteroid.r, -asteroid.r)
+	elseif angle < 1.5 * math.pi then
+		asteroid.pos = Vector(asteroid.r + w, asteroid.r + h)
+	else
+		asteroid.pos = Vector(asteroid.r, asteroid.r + h)
+	end
+	return asteroid
+end
+
+AsteroidsCtrl = Class {
+	init = function(self)
+		self.asteroids = {
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+			randomAsteroid(),
+		}
+	end,
+}
+
+function AsteroidsCtrl:update(dt)
+	for i, v in ipairs(self.asteroids) do
+		v.pos = v.pos + v.vel * dt
+	end
+end 
+
+function AsteroidsCtrl:draw()
+	for i, v in ipairs(self.asteroids) do
+		love.graphics.setColor(255, 255, 255)
+		love.graphics.circle('fill', v.pos.x, v.pos.y, v.r, 40)
+	end
+end
+
+return AsteroidsCtrl
--- game.lua
+++ game.lua
@@ -4,20 +4,25 @@ local states = require "states"
 states.game = game
 
 Ship = require "ship"
+AsteroidsCtrl = require "asteroidsctrl"
 Vector = require "hump.vector"
 
 function game:enter()
 end
 
 function game:init()
+	math.randomseed(os.time())
 	self.ship = Ship(Vector(100, 100))
+	self.asteroidsCtrl = AsteroidsCtrl()
 end
 
 function game:draw()
+	self.asteroidsCtrl:draw()
 	self.ship:draw()
 end
 
 function game:update(dt)
+	self.asteroidsCtrl:update(dt)
 	self.ship:update(dt)
 end
 
